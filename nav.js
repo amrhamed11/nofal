27eef2dbcb37c513e066903c44643f830e1d0c3a
amrhamed11 <amrrhamed27@gmail.com>
2023-10-15 18:46:25 +0300

    first

---
+++ nav.js
@@ -0,0 +1,198 @@
+let slaid1 = document.getElementById('slad1')
+let slide2 = document.getElementById('slad2')
+let slide3 = document.getElementById('slad3')
+let slide4 = document.getElementById('slad4')
+let slide5 = document.getElementById('slad5')
+
+
+
+function opennav(){
+    document.getElementById('box').style.display = 'block'    
+    document.querySelector('.nofalseo').style.opacity = '0.8'
+}
+
+
+window.addEventListener('mouseup' , function(event){
+    let menu = document.getElementById('box')
+    let show = this.document.querySelector('.nofalseo')
+    if(event.target != menu){
+        menu.style.display = 'none'
+        show.style.opacity = '1'
+    }
+
+})
+
+
+
+function slaida(){
+    document.getElementById('slida').style.display = 'block'
+}
+function slaidb(){
+    document.getElementById('slidb').style.display = 'block'
+}
+function slaidc(){
+    document.getElementById('slidc').style.display = 'block'
+}
+function slaidd(){
+    document.getElementById('slidd').style.display = 'block'
+}
+function slaide(){
+    document.getElementById('slide').style.display = 'block'
+}
+
+
+
+
+
+let click;
+function display11(){
+    if(click==1){
+        document.querySelector('.text11').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text11').style.display = 'none'
+        return click=1
+    }
+}
+
+function display12(){
+    if(click==1){
+        document.querySelector('.text12').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text12').style.display = 'none'
+        return click=1
+    }
+}
+function display13(){
+    if(click==1){
+        document.querySelector('.text13').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text13').style.display = 'none'
+        return click=1
+    }
+}
+function display14(){
+    if(click==1){
+        document.querySelector('.text14').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text14').style.display = 'none'
+        return click=1
+    }
+}
+function display15(){
+    if(click==1){
+        document.querySelector('.text15').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text15').style.display = 'none'
+        return click=1
+    }
+}
+function display21(){
+    if(click==1){
+        document.querySelector('.text21').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text21').style.display = 'none'
+        return click=1
+    }
+}
+function display22(){
+    if(click==1){
+        document.querySelector('.text22').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text22').style.display = 'none'
+        return click=1
+    }
+}
+function display23(){
+    if(click==1){
+        document.querySelector('.text23').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text23').style.display = 'none'
+        return click=1
+    }
+}
+function display24(){
+    if(click==1){
+        document.querySelector('.text24').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text24').style.display = 'none'
+        return click=1
+    }
+}
+function display25(){
+    if(click==1){
+        document.querySelector('.text25').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text25').style.display = 'none'
+        return click=1
+    }
+}
+function display31(){
+    if(click==1){
+        document.querySelector('.text31').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text31').style.display = 'none'
+        return click=1
+    }
+}
+function display32(){
+    if(click==1){
+        document.querySelector('.text32').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text32').style.display = 'none'
+        return click=1
+    }
+}
+function display33(){
+    if(click==1){
+        document.querySelector('.text33').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text33').style.display = 'none'
+        return click=1
+    }
+}
+function display34(){
+    if(click==1){
+        document.querySelector('.text34').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text34').style.display = 'none'
+        return click=1
+    }
+}
+function display35(){
+    if(click==1){
+        document.querySelector('.text35').style.display = 'block'
+        return click=0
+    }
+    else{
+        document.querySelector('.text35').style.display = 'none'
+        return click=1
+    }
+}
